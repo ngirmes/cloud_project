6344c3e161be9489eb204931231207131f5cd1ff
ngirmes <n.girmes1998@gmail.com>
2024-10-28 00:40:34 -0700

    deleted unecessary files

--- create_account.ps1
+++
@@ -1,4 +0,0 @@
-Invoke-WebRequest http://127.0.0.1:5000/create_account `
-    -Method Post `
-    -Headers @{"Content-Type" = "application/json" } `
-    -Body (Get-Content -Raw -Path "account_info.json")
